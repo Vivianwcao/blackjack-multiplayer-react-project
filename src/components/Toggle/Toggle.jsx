860b8da2d9cdd996d7760c3fcaf621c7507395a4
Vivian <59620790+Vivianwcao@users.noreply.github.com>
2025-05-19 12:00:13 -0700

    final touches

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -1,10 +1,15 @@
 import "./Toggle.scss";
+import { RiPokerSpadesFill } from "react-icons/ri";
 
 export default function Toggle({ isOn, setIsOn, onLabel, offLabel }) {
 	return (
 		<div className={`toggle toggle--${isOn ? "on" : "off"}`} onClick={setIsOn}>
 			<span className="toggle__label">{isOn ? onLabel : offLabel}</span>
-			<div className={`toggle__knob toggle__knob--${isOn ? "on" : ""}`} />
+			{/* <div className={`toggle__knob toggle__knob--${isOn ? "on" : ""}`}> */}
+			<RiPokerSpadesFill
+				className={`toggle__knob toggle__knob--${isOn ? "on" : ""}`}
+			/>
+			{/* </div> */}
 		</div>
 	);
 }
